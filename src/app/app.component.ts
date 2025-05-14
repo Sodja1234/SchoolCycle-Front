3ab104fca318379751973efcae9c0cff63cdfafb
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-14 17:56:19 +0200

    Ajout du header avec le logo et la barre de navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { HeaderComponent } from './pages/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
