b1a80048de8a47a49a9f4027e1929456654eac51
Exauce Bodi <exaucebodi4@gmail.com>
2025-05-26 14:06:02 +0200

    Amélioration du carrousel et rendu responsive de la page d'accueil

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -4,15 +4,15 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { AnnouncementListComponent } from '../../announcement/announcement-list/announcement-list.component';
 import { initFlowbite } from 'flowbite';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home',
-  imports: [AnnouncementListComponent, FooterComponent,HeaderComponent],
+  imports: [AnnouncementListComponent, FooterComponent,HeaderComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
  ngOnInit(): void {
-    //alert('Hello World!')
     initFlowbite();
     console.log('INIT')
   }
