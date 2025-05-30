48c93aeb4912a1fb18d4938233a7a86608fdf6f0
29delia <155080499+29delia@users.noreply.github.com>
2025-05-30 14:44:51 +0100

    creation du composant announcement profile

---
+++ src/app/pages/profile/announcement-profile/announcement-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-profile',
+  imports: [],
+  templateUrl: './announcement-profile.component.html',
+  styleUrl: './announcement-profile.component.css'
+})
+export class AnnouncementProfileComponent {
+
+}
