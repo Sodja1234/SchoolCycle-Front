fc4f0b59671705a41753b559bc1e50218def76aa
29delia <155080499+29delia@users.noreply.github.com>
2025-06-03 12:42:01 +0100

    Intégration de la page user setting

--- src/app/pages/profile/user-profile/user-profile.component.ts
+++ src/app/pages/profile/user-profile/user-profile.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
