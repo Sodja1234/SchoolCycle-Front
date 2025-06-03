80e32f15198520b9ab5325fd378ed559e57ff51e
alainkatayi <alainkatayi01@gmail.com>
2025-06-03 15:54:32 +0200

    feat: affichage dynamique des annonces sous forme de liste

--- src/app/pages/announcement/announcement-list/announcement-list.component.ts
+++ src/app/pages/announcement/announcement-list/announcement-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { AnnouncementCardComponent } from '../announcement-card/announcement-card.component';
+import { AnnouncementCardComponent } from '../../../components/announcement-card/announcement-card.component';
+import { Announcement } from '../../../core/models/announcement/announcement';
+import { AnnouncementService } from '../../../core/services/announcement/announcement.service';
 
 
 @Component({
@@ -9,5 +11,24 @@ import { AnnouncementCardComponent } from '../announcement-card/announcement-car
   styleUrl: './announcement-list.component.css'
 })
 export class AnnouncementListComponent {
+  announcements!: Announcement[]
 
-}
+  constructor(private announcementService:AnnouncementService){}
+
+
+  ngOnInit(){
+    this.getAnnouncements();
+  }
+
+  getAnnouncements(){
+    this.announcementService.getAnnouncements().subscribe({
+      next:(res)=>{
+        this.announcements = res.data;
+        console.log("Annonces", this.announcements)
+      }
+    });
+  }
+  
+
+  
+} 
