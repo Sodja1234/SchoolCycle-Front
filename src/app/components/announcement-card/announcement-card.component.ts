5def4a1c75c370abe5334a5f5ca1cda7788c737f
alainkatayi <alainkatayi01@gmail.com>
2025-06-03 15:46:58 +0200

    feat: récupération et affichage des articles dans le composants Card

--- src/app/components/announcement-card/announcement-card.component.ts
+++ src/app/components/announcement-card/announcement-card.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AnnouncementService } from '../../core/services/announcement/announcement.service';
+import { Announcement } from '../../core/models/announcement/announcement';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-announcement-card',
-  imports: [],
+  imports: [RouterModule ],
   templateUrl: './announcement-card.component.html',
-  styleUrl: './announcement-card.component.css'
+  styleUrl: './announcement-card.component.css',
 })
 export class AnnouncementCardComponent {
-
+  announcementId!: number;
+  constructor(private announcementService: AnnouncementService) {}
+  @Input() announcement!: Announcement;
+  ngOnInit() {
+    console.log('Photo recuperer', this.announcement.photos[0]?.url);
+  }
 }
--- src/app/core/services/announcement/announcement.service.ts
+++ src/app/core/services/announcement/announcement.service.ts
@@ -3,24 +3,29 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { Announcement } from '../../models/announcement/announcement';
+import { Category } from '../../models/announcement/category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnnouncementService {
-
   private url = environment.apiUrl;
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   //methode pour recuperer les annonces
-  getAnnouncements(): Observable<{ data: Announcement }> {
-    const announcements = this.http.get<{ data: Announcement }>(this.url + 'announcements');
+  getAnnouncements(): Observable<{ data: Announcement[] }> {
+    const announcements = this.http.get<{ data: Announcement[] }>(
+      this.url + 'announcements'
+    );
     return announcements;
   }
 
-  getAnnoucement(id: number) :Observable<{ data: Announcement }> {
-    const announcement = this.http.get<{ data: Announcement }>(this.url + 'announcements/' + id);
-    return announcement;
+  getAnnoucement(id: number): Observable<Announcement> {
+    return this.http.get<Announcement>(this.url + 'announcements/' + id);
   }
 
+  //On recupere les categories
+  getCategories(): Observable<{ data: any[] }> {
+    return this.http.get<{ data: any[] }>(`${this.url}categories`);
+  }
 }
