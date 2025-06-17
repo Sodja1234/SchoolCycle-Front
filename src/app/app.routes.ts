e47a18bab77b25284e783d4fbe79bdf117b65caf
alainkatayi <alainkatayi01@gmail.com>
2025-06-17 19:05:33 +0200

    Ajout de la route edit-announcement pour la modification d'une annonce

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,29 +2,40 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path : '',
-    loadComponent:() => import('../app/pages/home/home/home.component').then((m)=> m.HomeComponent),
+    path: '',
+    loadComponent: () =>
+      import('../app/pages/home/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
   },
   {
-    path : 'register',
-    loadComponent:() => import('../app/pages/auth/register/register.component').then((m) => m.RegisterComponent),
+    path: 'register',
+    loadComponent: () =>
+      import('../app/pages/auth/register/register.component').then(
+        (m) => m.RegisterComponent
+      ),
   },
   {
-    path : 'login',
-    loadComponent:() => import('../app/pages/auth/login/login.component').then((m) => m.LoginComponent),
+    path: 'login',
+    loadComponent: () =>
+      import('../app/pages/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
   },
   {
-    path : 'verify-email/:id/:hash',
-    loadComponent : ()=>
-      import('./pages/auth/verify-email/verify-email.component').then((m)=> m.VerifyEmailComponent)
+    path: 'verify-email/:id/:hash',
+    loadComponent: () =>
+      import('./pages/auth/verify-email/verify-email.component').then(
+        (m) => m.VerifyEmailComponent
+      ),
   },
-  
+
   {
     path: 'forgot-password',
     loadComponent: () =>
-    import('./pages/auth/reset-password/reset-password.component').then(
-      (m) => m.ResetPasswordComponent
-    ),
+      import('./pages/auth/reset-password/reset-password.component').then(
+        (m) => m.ResetPasswordComponent
+      ),
   },
   {
     path: 'password-reset/:token',
@@ -34,34 +45,52 @@ export const routes: Routes = [
       ),
   },
   {
-    path:'profils',
-    loadComponent: () => import('../app/pages/profile/profils/profils.component').then((m) => m.ProfilsComponent),
+    path: 'profils',
+    loadComponent: () =>
+      import('../app/pages/profile/profils/profils.component').then(
+        (m) => m.ProfilsComponent
+      ),
   },
   {
-    path:'user-setting',
-    loadComponent:() => import('../app/pages/profile/user-setting/user-setting.component').then((m) => m.UserSettingComponent),
-  }
-  ,
-
+    path: 'user-setting',
+    loadComponent: () =>
+      import('../app/pages/profile/user-setting/user-setting.component').then(
+        (m) => m.UserSettingComponent
+      ),
+  },
   {
     path: 'chat',
     loadComponent: () =>
       import('../app/pages/chat/chat-container/chat-container.component').then(
         (m) => m.ChatContainerComponent
-      )
-  }
-  ,
-  {
-    path:'create-announcement',
-    loadComponent:() => import('../app/pages/announcement/announcement-create/announcement-create.component').then((m) => m.AnnouncementCreateComponent),
+      ),
   },
   {
-    path:'single-announcement/:id',
-    loadComponent:() => import('../app/pages/announcement/announcement-single/announcement-single.component').then((m) => m.AnnouncementSingleComponent),
+    path: 'create-announcement',
+    loadComponent: () =>
+      import(
+        '../app/pages/announcement/announcement-create/announcement-create.component'
+      ).then((m) => m.AnnouncementCreateComponent),
   },
   {
-    path:'announcement-gallery',
-    loadComponent:()=>import('../app/pages/announcement/announcement-gallery/announcement-gallery.component').then((m)=>m.AnnouncementGalleryComponent)
-  }
-
+    path: 'edit-announcement/:id',
+    loadComponent: () =>
+      import(
+        '../app/pages/announcement/announcement-edit/announcement-edit.component'
+      ).then((m) => m.AnnouncementEditComponent),
+  },
+  {
+    path: 'single-announcement/:id',
+    loadComponent: () =>
+      import(
+        '../app/pages/announcement/announcement-single/announcement-single.component'
+      ).then((m) => m.AnnouncementSingleComponent),
+  },
+  {
+    path: 'announcement-gallery',
+    loadComponent: () =>
+      import(
+        '../app/pages/announcement/announcement-gallery/announcement-gallery.component'
+      ).then((m) => m.AnnouncementGalleryComponent),
+  },
 ];
