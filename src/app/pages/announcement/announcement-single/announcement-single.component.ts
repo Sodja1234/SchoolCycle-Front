3967f23f17ef789aaec8032dac57d9fc9e97cfa7
alainkatayi <alainkatayi01@gmail.com>
2025-06-18 14:42:29 +0200

    Ajout du bouton modifié et Création d'un getter qui verifie si l'annonce a été modifiée ou non

--- src/app/pages/announcement/announcement-single/announcement-single.component.ts
+++ src/app/pages/announcement/announcement-single/announcement-single.component.ts
@@ -3,18 +3,19 @@ import { Announcement } from '../../../core/models/announcement/announcement';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { AnnouncementService } from '../../../core/services/announcement/announcement.service';
 import { environment } from '../../../../environments/environment';
-import { HeaderComponent } from "../../../components/header/header.component";
-import { FooterComponent } from "../../../components/footer/footer.component";
+import { HeaderComponent } from '../../../components/header/header.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { formatDistanceToNow } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 @Component({
   selector: 'app-announcement-single',
   imports: [RouterLink, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './announcement-single.component.html',
-  styleUrl: './announcement-single.component.css'
+  styleUrl: './announcement-single.component.css',
 })
 export class AnnouncementSingleComponent {
-
   constructor(
     private annoncementService: AnnouncementService,
     private route: ActivatedRoute
@@ -24,10 +25,15 @@ export class AnnouncementSingleComponent {
   announcement!: Announcement;
   similarAnnouncements: Announcement[] = [];
   articleId: number = -1;
+  announcementData!: Announcement;
+  currentUserId: number = -1;
 
   ngOnInit() {
+    // Récupère l'ID de l'utilisateur connecté une seule fois
+    const user = JSON.parse(localStorage.getItem('user')!);
+    this.currentUserId = user?.id;
     //recharger la page en dunction du nouvel id
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.articleId = +params['id'];
       this.getSingleAnnouncement();
       this.getSimilarAnnouncement();
@@ -35,7 +41,7 @@ export class AnnouncementSingleComponent {
   }
 
   //function pour recuperr l'annonce en detail
-  getSingleAnnouncement(){
+  getSingleAnnouncement() {
     this.articleId = Number(this.route.snapshot.paramMap.get('id'));
 
     console.log("Id de l'annonce : ", this.articleId);
@@ -43,7 +49,12 @@ export class AnnouncementSingleComponent {
     this.annoncementService.getAnnoucement(this.articleId).subscribe({
       next: (res) => {
         this.announcement = res;
+        this.currentUserId = +localStorage.getItem('id')!;
         console.log('Annonce récupérée :', this.announcement);
+        console.log('ID auteur :', this.announcement.created_by);
+        console.log('ID user connecté :', this.currentUserId);
+        console.log(this.announcement.created_at_raw);
+        console.log(this.announcement.updated_at_raw);
         // Vérifie que des photos existent, et affecte currentImage
         if (this.announcement.photos && this.announcement.photos.length > 0) {
           this.currentImage = this.announcement.photos[0].url;
@@ -54,8 +65,6 @@ export class AnnouncementSingleComponent {
       },
     });
   }
-
-  
   //function pour recuperr les articles similaires
   getSimilarAnnouncement() {
     this.annoncementService.getSimilarAnnouncements(this.articleId).subscribe(
@@ -73,5 +82,23 @@ export class AnnouncementSingleComponent {
   changeMainImage(url: string) {
     this.currentImage = url;
   }
-}  
- 
+
+  //Vérifie si le user connecté est l'auteur
+  get isAuthor(): boolean {
+    const userId = +localStorage.getItem('id')!;
+    return this.announcement?.created_by?.id === userId;
+  }
+
+  get isModified(): boolean {
+    if (
+      !this.announcement?.created_at_raw ||
+      !this.announcement?.updated_at_raw
+    )
+      return false;
+
+    const created = new Date(this.announcement.created_at_raw).getTime();
+    const updated = new Date(this.announcement.updated_at_raw).getTime();
+
+    return created !== updated;
+  }
+}
