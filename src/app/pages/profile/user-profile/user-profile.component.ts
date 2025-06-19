72d5f2bb80de5797f6bb1a4c709127440975248a
zokol <eunicezokol5@gmail.com>
2025-06-19 12:17:11 +0100

    ajout de la methode pour recuperer les info de l'utilisateur connecté

--- src/app/pages/profile/user-profile/user-profile.component.ts
+++ src/app/pages/profile/user-profile/user-profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserLocalService } from '../../../core/services/userlocal/userlocal.service';
+import { User } from '../../../core/models/user';
+import { AuthLoginResponse } from '../../../core/models/auth/auth';
+import { ActionRapideComponent } from "../action-rapide/action-rapide.component";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [RouterLink],
+  imports: [RouterLink, ActionRapideComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+user!:AuthLoginResponse|null;
+  constructor(private userLocalService:UserLocalService){
 
+  }
+  ngOnInit(){
+    this.user=this.userLocalService.getUser();
+}
 }
