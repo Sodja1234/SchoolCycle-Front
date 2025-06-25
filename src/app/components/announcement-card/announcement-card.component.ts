035a4cb49d1c5efa8ee42ffda2affb8893a9a13f
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-25 14:09:14 +0200

    Ajout de la logique de gestion des favoris dans le composant AnnouncementCard

--- src/app/components/announcement-card/announcement-card.component.ts
+++ src/app/components/announcement-card/announcement-card.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { AnnouncementService } from '../../core/services/announcement/announcement.service';
 import { Announcement } from '../../core/models/announcement/announcement';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { UserLocalService } from '../../core/services/userlocal/userlocal.service';
+import { AuthLoginResponse } from '../../core/models/auth/auth';
+import { FavoriteStateService } from '../../core/services/favorite/favorite.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-announcement-card',
@@ -11,10 +15,45 @@ import { CommonModule } from '@angular/common';
   styleUrl: './announcement-card.component.css',
 })
 export class AnnouncementCardComponent {
+
   announcementId!: number;
-  constructor(private announcementService: AnnouncementService) {}
+  isFavorite$ : Observable<boolean> | undefined; // Observable pour l'état du favori
+  errorMessage: string = '';
+  successMessage: string = '';
+  user : AuthLoginResponse | null = null;
+  favoriteMessage: string = '';
+  showSucessMessage: boolean = false;
+  messageTimeout: any;
+
+  constructor(private announcementService: AnnouncementService, public userLocalService : UserLocalService, private favoriteState: FavoriteStateService) {}
   @Input() announcement!: Announcement;
+
   ngOnInit() {
+    this.user = this.userLocalService.getUser();
+    this.isFavorite$ = this.favoriteState.isFavorite(this.announcement.id);
+
+    // Vérifiez l'état initial
+    this.announcementService.checkFavorite(this.announcement.id).subscribe();
     console.log('Photo recuperer', this.announcement.photos[0]?.url);
   }
+
+   toggleFavorite() {
+    this.announcementService.toggleFavorite(this.announcement.id).subscribe({
+      next: (res: any) => {
+        // Affiche le message de succès
+        this.successMessage = res.message;
+        this.showSucessMessage = true;
+
+        // Cache le message après 3 secondes
+        clearTimeout(this.messageTimeout);
+        this.messageTimeout = setTimeout(() => {
+          this.showSucessMessage = false;
+        }, 3000);
+      },
+      error: (err) => {
+        console.error('Erreur lors du toggle favorite:', err);
+      }
+    });
+  }
+
 }
