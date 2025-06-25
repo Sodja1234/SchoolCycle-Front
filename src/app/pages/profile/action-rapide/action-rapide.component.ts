10e469f9e2946183d314d15de612609a6d8c0a53
zokol <eunicezokol5@gmail.com>
2025-06-25 14:50:19 +0100

    implementation des bouttons dans les composant action-rapide

--- src/app/pages/profile/action-rapide/action-rapide.component.ts
+++ src/app/pages/profile/action-rapide/action-rapide.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {  Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-action-rapide',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './action-rapide.component.html',
   styleUrl: './action-rapide.component.css'
 })
 export class ActionRapideComponent {
+  isConnected:boolean=false;
+  constructor(private router:Router){}
+
+logOut() {
+console.log('Clique que sur le button')
+    localStorage.clear();
+    this.isConnected = false ;
+    this.router.navigate(['/']).then(()=>{
+      location.reload();
+    });
+}
 
 }
