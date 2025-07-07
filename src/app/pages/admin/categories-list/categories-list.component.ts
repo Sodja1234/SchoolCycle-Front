685f3ff141e92a27f1a1284d8bc057bea0bdbdfb
alainkatayi <alainkatayi01@gmail.com>
2025-07-07 14:30:37 +0200

    Creation du composant categorie-list, qui affiche  toutes les categories dans l'interface admin

---
+++ src/app/pages/admin/categories-list/categories-list.component.ts
@@ -0,0 +1,32 @@
+import { Component} from '@angular/core';;
+import { Category } from '../../../core/models/announcement/category';
+
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-categories-list',
+  imports: [CommonModule, RouterLink ],
+  templateUrl: './categories-list.component.html',
+  styleUrl: './categories-list.component.css',
+})
+export class CategoriesListComponent {
+  categories!: Category[];
+
+  constructor( private categorieService: CategoriesService) {}
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  // Récuperation des catégories depuis l'API via le service categorie
+  getCategories() {
+    this.categorieService.getCategories().subscribe({
+      next: (res) => {
+        this.categories = res.data; 
+        console.log('Catégories:', this.categories); // Debug
+      },
+    });
+  }
+
+}
