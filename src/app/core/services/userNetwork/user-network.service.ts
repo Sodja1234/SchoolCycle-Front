f6173ea8b86b08a921554cb4272229897417bca4
alainkatayi <alainkatayi01@gmail.com>
2025-07-09 20:40:36 +0200

    Intégration  de la pagination dans la methode getUser avec retour de PaginatedUser, dans UserServiceNetwork

--- src/app/core/services/userNetwork/user-network.service.ts
+++ src/app/core/services/userNetwork/user-network.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user';
 import { UserLocalService } from '../userlocal/userlocal.service';
+import { PaginatedUser } from '../../models/announcement/pagination';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +13,11 @@ export class UserNetworkService {
   private url = environment.apiUrl;
   constructor(private http: HttpClient,private userlocalService : UserLocalService) {}
 
-  getUser(): Observable<{ data: User[] }> {
+  getUser(page: number = 1): Observable<PaginatedUser> {
+    let params = new HttpParams()
+      .set('page', page)
+      .set('per_page', 10);
     const headers = this.userlocalService.getAuthHeaders();
-    return this.http.get<{ data: User[] }>(this.url + 'users', { headers });
+    return this.http.get<PaginatedUser>(this.url + 'users', { params });
   }
 }
