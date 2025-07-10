60817d18eabf8f455fc64f9efc86c48d182df99e
alainkatayi <alainkatayi01@gmail.com>
2025-07-10 17:04:12 +0200

    Ajout du modal de confirmation et implémentation de la désactivation d'une annonce depuis le composant admin report-card via le service

--- src/app/components/report-card/report-card.component.ts
+++ src/app/components/report-card/report-card.component.ts
@@ -1,15 +1,47 @@
 import { Component, Input } from '@angular/core';
 import { AnnouncementService } from '../../core/services/announcement/announcement.service';
 import { Report } from '../../core/models/announcement/report';
+import { Announcement } from '../../core/models/announcement/announcement';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-report-card',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './report-card.component.html',
-  styleUrl: './report-card.component.css'
+  styleUrl: './report-card.component.css',
 })
 export class ReportCardComponent {
-    reportId!: number;
-  constructor(private reportService: AnnouncementService) {}
+  reportId!: number;
+  articleId: number = -1;
+  isModalOpen = false;
+  isDeleteModalOpen = false;
+  constructor(private announcementService: AnnouncementService) {}
   @Input() report!: Report;
+  @Input() announcement!: Announcement;
+
+  openDeleteModal(id: number) {
+    this.isDeleteModalOpen = true;
+    this.articleId = id;
+  }
+
+  closeDeleteModal() {
+    this.isDeleteModalOpen = false;
+  }
+
+  confirmDelete() {
+    this.deleteAnnouncement();
+    this.closeDeleteModal();
+  }
+
+  deleteAnnouncement() {
+    this.announcementService.deleteAnnouncement(this.articleId).subscribe({
+      next: () => {
+        window.location.reload();
+      },
+      error: (err) => {
+        console.error('Erreur suppression annonce', err);
+      },
+    });
+  }
 }
