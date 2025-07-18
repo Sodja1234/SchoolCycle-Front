acea9d5933467566f9cd7a93f1ba260808133096
zokol <eunicezokol5@gmail.com>
2025-07-18 16:42:20 +0100

    ajoutd de la pagination pour les favories dans le composant announcement-favorite

--- src/app/pages/profile/announcemevntuser/announcement-favorite/announcement-favorite.component.ts
+++ src/app/pages/profile/announcemevntuser/announcement-favorite/announcement-favorite.component.ts
@@ -2,26 +2,35 @@ import { Component } from '@angular/core';
 import { AnnouncementService } from '../../../../core/services/announcement/announcement.service';
 import { Announcement } from '../../../../core/models/announcement/announcement';
 import { AnnouncementCardComponent } from '../../../../components/announcement-card/announcement-card.component';
+import { PaginationMeta, PaginationUrls } from '../../../../core/models/announcement/pagination';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-announcement-favorite',
-  imports: [AnnouncementCardComponent],
+  imports: [AnnouncementCardComponent, CommonModule],
   templateUrl: './announcement-favorite.component.html',
   styleUrl: './announcement-favorite.component.css'
 })
 export class AnnouncementFavoriteComponent {
   favorites!:Announcement[];
+   paginationMeta!: PaginationMeta;
+      paginationUrls!: PaginationUrls;
 constructor(private announcementService:AnnouncementService){
 
 }
 ngOnInit(){
   this.getAnnouncementFavorite();
 }
-getAnnouncementFavorite(){
-  this.announcementService.getAnnouncementFavorite().subscribe({
+getAnnouncementFavorite(page:number =1){
+  this.announcementService.getAnnouncementFavorite(page).subscribe({
     next:(res)=>{
       this.favorites=res.data;
       console.log('recuperé les favories',this.favorites)
+      this.paginationMeta = res.meta;
+
+        //on stocke les annonces recus dans la varible links de PaginatedAnnouncements !!
+      this.paginationUrls = res.links;
+
     },
     error:(err)=>{
       console.error('error:',err)
@@ -31,4 +40,19 @@ getAnnouncementFavorite(){
 
 
 }
+
+//methode utiliser lorque l'utilisateur clique un lien  de la pagination
+  onPageChange(url: string | null | undefined): void {
+    //si l'url n'est pas valide, on return rien
+    if (typeof url !== 'string') return;
+
+    //on extrait  le parametre page depuis l'url
+    const pageParam = new URL(url).searchParams.get('page');
+
+    //on converti la valeur page en nombre
+    const page = pageParam ? +pageParam : 1;
+
+    //on renvoit les annonces pour la page selectionné
+    this.getAnnouncementFavorite(page);
+  }
 }
