cda634ce738a92d2e2ef7a2cb43c97b39c502798
Delice <delicelombeto18@.com>
2025-07-23 11:58:18 +0100

    ajout de la logique  de  modification des informations du profil utilisateur dans le composant user-profile-info

--- src/app/pages/profile/user-profile-info/user-profile-info.component.ts
+++ src/app/pages/profile/user-profile-info/user-profile-info.component.ts
@@ -1,11 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Profile } from '../../../core/models/profile/profile';
+import { AuthLoginResponse } from '../../../core/models/auth/auth';
+import { ProfileService } from '../../../core/services/profile/profile.service';
+import { UserLocalService } from '../../../core/services/userlocal/userlocal.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile-info',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-profile-info.component.html',
-  styleUrl: './user-profile-info.component.css'
+  styleUrls: ['./user-profile-info.component.css']
 })
-export class UserProfileInfoComponent {
+export class UserProfileInfoComponent implements OnInit {
+  profileForm!: FormGroup;
+  userProfile!: Profile | undefined;
+  user!: AuthLoginResponse | null;
+  avatarUrl: string | undefined;
+ 
 
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService,
+    private userLocalService: UserLocalService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      name: [''],
+      telephone: [''],
+      bio: [''],
+      adresse: [''],
+      profession: [''],
+       avatar: ['']
+    });
+
+    this.user = this.userLocalService.getUser();
+    this.getProfileTutor();
+  }
+
+  getProfileTutor(): void {
+    this.profileService.getProfileTutor().subscribe({
+      next: (response) => {
+        const userData = response.data.user;
+        this.avatarUrl = userData.avatarUrl || ''; 
+        this.profileForm.patchValue({
+          name: userData.name,
+          telephone: response.data.telephone,
+          bio: response.data.bio,
+          adresse: response.data.adresse,
+          profession: response.data.profession,
+          avatar: null
+        });
+        this.userProfile = response.data;
+        console.log('Profil utilisateur récupéré:', this.userProfile);
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération du profil utilisateur: ', error);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    if (this.profileForm.invalid) {
+      console.warn('Formulaire invalide');
+      return;
+    }
+    const formData = new FormData();
+    const formValue = this.profileForm.value;
+
+    formData.append('name', formValue.name || '');
+    formData.append('telephone', formValue.telephone || '');
+    formData.append('bio', formValue.bio || '');
+    formData.append('adresse', formValue.adresse || '');
+    formData.append('profession', formValue.profession || '');
+
+    if (formValue.avatar) {
+      formData.append('avatar', formValue.avatar);
+    }
+
+    this.profileService.updateProfile(formData).subscribe({
+      next: () => {
+        console.log('Profil mis à jour');
+        this.getProfileTutor(); // Recharge les données pour mise à jour visuelle
+        this.router.navigate(['/profils']);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la mise à jour :', err);
+      }
+    });
+  }
+
+  onFileChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.profileForm.patchValue({ avatar: file });
+      this.profileForm.get('avatar')?.updateValueAndValidity();
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result) {
+          this.avatarUrl = reader.result as string; // Met à jour la prévisualisation
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
