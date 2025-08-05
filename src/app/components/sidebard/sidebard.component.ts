b774df8b8f38129b80acc47723818022796b4c78
alainkatayi <alainkatayi01@gmail.com>
2025-08-05 18:15:16 +0200

    Ajout des onglets Aller sur le site Déconnexion dans la sidebar

--- src/app/components/sidebard/sidebard.component.ts
+++ src/app/components/sidebard/sidebard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthLoginResponse } from '../../core/models/auth/auth';
 import { UserLocalService } from '../../core/services/userlocal/userlocal.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebard',
@@ -12,11 +12,19 @@ import { RouterLink } from '@angular/router';
 export class SidebardComponent {
     user!: AuthLoginResponse | null;
     constructor(
-      private authservice: UserLocalService,
+      private authservice: UserLocalService,private router : Router
     ) {}
   
     ngOnInit() {
       this.user = this.authservice.getUser();
       console.log('user', this.user);
     }
+
+    logOut(){
+      console.log('Clique que sur le button')
+      localStorage.clear();
+      this.router.navigate(['/']).then(()=>{
+        location.reload();
+      });
+  }
 }
